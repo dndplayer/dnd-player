552ef596fa9edce07cbd1a33d5602b7f8ddbc288
Dom Light <bodricklight@gmail.com>
2019-04-24 22:34:06 +0100

    Updates

---
+++ precache-manifest.3377a6ea733a2c6188ecf90835f85f18.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "301ac636bf44bb1e6814ec89b9ffd4ed",
+    "url": "/dnd-player/static/media/disadvantage.301ac636.svg"
+  },
+  {
+    "revision": "d1cfaa3f7111001a298b882a03e6105e",
+    "url": "/dnd-player/static/media/advantage.d1cfaa3f.svg"
+  },
+  {
+    "revision": "0ddd88021f048d512ab21ad88b181a66",
+    "url": "/dnd-player/static/media/ac.0ddd8802.svg"
+  },
+  {
+    "revision": "b1adb866746b7a45723f",
+    "url": "/dnd-player/static/js/runtime~main.f58116c1.js"
+  },
+  {
+    "revision": "b64b12e3ccb807a2420a",
+    "url": "/dnd-player/static/js/main.15d85b89.chunk.js"
+  },
+  {
+    "revision": "9c146b80c8133253a1b7",
+    "url": "/dnd-player/static/js/2.fba44a76.chunk.js"
+  },
+  {
+    "revision": "b64b12e3ccb807a2420a",
+    "url": "/dnd-player/static/css/main.0a360331.chunk.css"
+  },
+  {
+    "revision": "6e902e95f7e380be5d4ebfd5972e473e",
+    "url": "/dnd-player/index.html"
+  }
+];
