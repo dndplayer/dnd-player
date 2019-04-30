014bb5148f0be8c31f5b465721090fbd28b323f9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-30 20:59:17 +0000

    Deploy dnd-player.com to github.com/dndplayer/dnd-player.git:gh-pages

---
+++ precache-manifest.a96e09fc115ecb724bc8369f4f789cda.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "87f9bee8fe95ec1b8610",
+    "url": "/static/css/main.2652fa01.chunk.css"
+  },
+  {
+    "revision": "87f9bee8fe95ec1b8610",
+    "url": "/static/js/main.3c196e8e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cd49478a055668c536c4",
+    "url": "/static/js/2.a8743a17.chunk.js"
+  },
+  {
+    "revision": "b36135ab1ba1cdac857a0f3207630008",
+    "url": "/static/media/ac.b36135ab.svg"
+  },
+  {
+    "revision": "d1cfaa3f7111001a298b882a03e6105e",
+    "url": "/static/media/advantage.d1cfaa3f.svg"
+  },
+  {
+    "revision": "301ac636bf44bb1e6814ec89b9ffd4ed",
+    "url": "/static/media/disadvantage.301ac636.svg"
+  },
+  {
+    "revision": "61f779f4509d7b1402031ad8da0043a7",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.41830673092e2666f7d692d724622c8d.js"
+  "/precache-manifest.a96e09fc115ecb724bc8369f4f789cda.js"
 );
 
 workbox.clientsClaim();
