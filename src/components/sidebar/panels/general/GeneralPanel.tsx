ec23d02840443f61b7be263943c9eb26bd8f908d
Chris Ella <chrisella88@gmail.com>
2019-05-08 17:02:54 +0100

    refactor(sidebar): add basic bg colour picker (not functioning yet) and add notes to general sidebar panel
    issue #41

--- src/components/sidebar/panels/general/GeneralPanel.tsx
+++ src/components/sidebar/panels/general/GeneralPanel.tsx
@@ -5,6 +5,31 @@ export default class GeneralPanel extends Component {
 		return (
 			<div>
 				<h1>General</h1>
+
+				<h2>TODO:</h2>
+				<ul>
+					<li>Map stage background colour</li>
+					<li>Room URL copy button</li>
+					<li>Logout button</li>
+				</ul>
+
+				<div>
+					<input
+						type="color"
+						id="stageBackground"
+						name="stageBackground"
+						value="#ffffff"
+						style={{
+							margin: '.4rem'
+						}}
+					/>
+					<label
+						htmlFor="stageBackground"
+						style={{ font: '1rem "Fira Sans", sans-serif' }}
+					>
+						Map Background Colour
+					</label>
+				</div>
 			</div>
 		);
 	}
