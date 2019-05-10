82f50fcdf398cb747c3b15633c509ead051670b4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-10 21:26:10 +0000

    Deploy dnd-player.com to github.com/dndplayer/dnd-player.git:gh-pages

---
+++ precache-manifest.7bd235d8fea33e1ea5f9a7fc8b66b5cc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f17f33f23177ae92b1c7",
+    "url": "/static/css/main.4da6d652.chunk.css"
+  },
+  {
+    "revision": "f17f33f23177ae92b1c7",
+    "url": "/static/js/main.d31ea651.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6dcf2f5cffe47f255866",
+    "url": "/static/js/2.3a6dd3fd.chunk.js"
+  },
+  {
+    "revision": "d1cfaa3f7111001a298b882a03e6105e",
+    "url": "/static/media/advantage.d1cfaa3f.svg"
+  },
+  {
+    "revision": "301ac636bf44bb1e6814ec89b9ffd4ed",
+    "url": "/static/media/disadvantage.301ac636.svg"
+  },
+  {
+    "revision": "b36135ab1ba1cdac857a0f3207630008",
+    "url": "/static/media/ac.b36135ab.svg"
+  },
+  {
+    "revision": "50b1cdd084281513529205c14d169ab2",
+    "url": "/index.html"
+  }
+];
