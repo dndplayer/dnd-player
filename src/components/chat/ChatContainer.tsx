d62611b5ad71ec19b707bdac25225c4ff2c1954c
Dom Light <bodricklight@gmail.com>
2019-05-15 23:58:27 +0100

    fix(chat): readd ability to pop out chat window

--- src/components/chat/ChatContainer.tsx
+++ src/components/chat/ChatContainer.tsx
@@ -8,6 +8,9 @@ import { login } from '../../redux/actions/auth';
 import ChatDisplay from './ChatDisplay';
 import { updateTime } from '../../redux/actions/globalState';
 import { getRecentMessages } from '../../redux/selectors/chat';
+import WindowPortal from '../util/WindowPortal';
+import { Icon } from '@material-ui/core';
+import { Rnd } from 'react-rnd';
 
 const mapStateToProps = (state): any => ({
 	messages: state.chat.messages,
@@ -47,12 +50,83 @@ interface StateFromProps {
 
 type Props = DispatchFromProps & StateFromProps;
 
-class ChatContainer extends Component<Props> {
+interface State {
+	showWindowPortal: boolean;
+}
+
+class ChatContainer extends Component<Props, State> {
+	state = {
+		showWindowPortal: false
+	};
+
+	private _chat: Chat;
+
+	componentDidUpdate(prevProps, prevState) {
+		if (this.state.showWindowPortal && !prevState.showWindowPortal) {
+			// this._chat.forceUpdate();
+			this.forceUpdate();
+		}
+	}
 	render(): ReactNode {
+		const chat = (
+			<Chat
+				{...this.props}
+				ref={c => (this._chat = c)}
+				messagesOpen={this.props.messagesOpen || this.state.showWindowPortal}
+			/>
+		);
 		return (
-			<div>
-				<Chat {...this.props} />
-				<ChatDisplay {...this.props} />
+			<div style={{ height: '100%' }}>
+				{!this.state.showWindowPortal ? (
+					// TODO: All this view specific code e.g. RND shouldn't be in the container!
+					// Containers are for Data, so move all this into a view based component.
+					<Rnd
+						default={{
+							x: 440,
+							y: -640,
+							width: '500px',
+							height: '600px'
+						}}
+						minWidth={200}
+						minHeight={100}
+						disableDragging={true}
+						style={{
+							zIndex: 1
+						}}
+					>
+						<div
+							style={{
+								height: '100%',
+								position: 'relative',
+								overflow: 'hidden'
+							}}
+						>
+							{this.props.messagesOpen && chat}
+							<ChatDisplay {...this.props} />
+							{this.props.messagesOpen && (
+								<Icon
+									style={{
+										position: 'absolute',
+										top: 5,
+										right: 5,
+										cursor: 'pointer',
+										fontSize: 18
+									}}
+									onClick={() => this.setState({ showWindowPortal: true })}
+								>
+									open_in_new
+								</Icon>
+							)}
+						</div>
+					</Rnd>
+				) : (
+					<WindowPortal
+						title="Chat"
+						onClose={() => this.setState({ showWindowPortal: false })}
+					>
+						{chat}
+					</WindowPortal>
+				)}
 			</div>
 		);
 	}
