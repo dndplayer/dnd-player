42dd56c6db0d8607e9ccb90c662c1cfa23e33824
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-16 23:38:46 +0000

    Deploy dnd-player.com to github.com/dndplayer/dnd-player.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.00bba14e95c05dd92edb77726d4fefd2.js"
+  "/precache-manifest.6fcd6ea3db5d8a04105bdfe5baad23f5.js"
 );
 
 workbox.clientsClaim();
