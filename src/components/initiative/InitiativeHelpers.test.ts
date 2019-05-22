664b6714e4c7c7df263cfe7e2cd25ef4cd546b42
Chris Ella <chrisella88@gmail.com>
2019-05-22 16:55:48 +0100

    refactor(initiatives): update init ordering to use dex for tie-breaks
    issue #6

--- src/components/initiative/InitiativeHelpers.test.ts
+++ src/components/initiative/InitiativeHelpers.test.ts
@@ -5,42 +5,58 @@
 import each from 'jest-each';
 import { orderInitiatives } from './InitiativeHelpers';
 import { InitiativeRoller } from '../../models/Initiative';
+import {
+	PlayerCharacter,
+	NonPlayerCharacter,
+	Character,
+	CharacterSize
+} from '../../5e/models/Character';
 
 describe('InitiativeHelpers', () => {
 	describe('orderInitiatives', () => {
 		it('should return an empty array if null or undefined is given', () => {
-			const x = orderInitiatives(null);
+			const x = orderInitiatives(null, [], []);
 			expect(x).toEqual([]);
 		});
-		it('should return an empty array if an empty array is given', () => {
+		it('should return an empty array if no playerCharacter or nonPlayerCharacter arrays are given', () => {
+			const x = orderInitiatives({}, null, null);
+			expect(x).toEqual([]);
+		});
+		it('should return an empty array if an empty object is given', () => {
 			const i = {};
-			const x = orderInitiatives(i);
+			const pc = generatePlayerCharacters([]);
+			const npc = generateNonPlayerCharacters([]);
+			const x = orderInitiatives(i, pc, npc);
 			expect(x).toEqual([]);
 		});
 		it('should return an array with current turn first', () => {
 			const i = {
 				currentTurn: '111',
 				rolls: {
-					'111': generateInitiative(),
-					'222': generateInitiative(),
-					'333': generateInitiative()
+					'111': generateInitiative(null, 'aaa'),
+					'222': generateInitiative(null, null, 'bbb'),
+					'333': generateInitiative(null, 'ccc')
 				}
 			};
-			const x = orderInitiatives(i);
+			const pc = generatePlayerCharacters(['aaa', 'ccc']);
+			const npc = generateNonPlayerCharacters(['bbb']);
+			const x = orderInitiatives(i, pc, npc);
 			expect(x[0].id).toEqual('111');
 		});
 		it('should return a correctly sorted array', () => {
 			const i = {
 				currentTurn: '333',
 				rolls: {
-					'111': generateInitiative(12),
-					'222': generateInitiative(5),
-					'333': generateInitiative(9),
-					'444': generateInitiative(19),
-					'555': generateInitiative(2)
+					'111': generateInitiative(12, 'aaa'),
+					'222': generateInitiative(5, 'bbb'),
+					'333': generateInitiative(9, 'ccc'),
+					'444': generateInitiative(19, null, 'ddd'),
+					'555': generateInitiative(2, 'eee')
 				}
 			};
-			const x = orderInitiatives(i);
+			const pc = generatePlayerCharacters(['aaa', 'bbb', 'ccc', 'eee']);
+			const npc = generateNonPlayerCharacters(['ddd']);
+			const x = orderInitiatives(i, pc, npc);
 
 			const y = x.map(z => z.initiativeRoll);
 			expect(y).toEqual([9, 5, 2, 19, 12]);
@@ -64,14 +80,16 @@ describe('InitiativeHelpers', () => {
 			const i = {
 				currentTurn: '111',
 				rolls: {
-					'111': generateInitiative(12),
-					'222': generateInitiative(5),
-					'333': generateInitiative(9),
-					'444': generateInitiative(19),
-					'555': generateInitiative(2)
+					'111': generateInitiative(12, 'aaa'),
+					'222': generateInitiative(5, null, 'bbb'),
+					'333': generateInitiative(9, null, 'ccc'),
+					'444': generateInitiative(19, null, 'ddd'),
+					'555': generateInitiative(2, 'eee')
 				}
 			};
-			const x = orderInitiatives(i);
+			const pc = generatePlayerCharacters(['aaa', 'eee']);
+			const npc = generateNonPlayerCharacters(['bbb', 'ccc', 'ddd']);
+			const x = orderInitiatives(i, pc, npc);
 
 			const y = x.map(z => z.initiativeRoll);
 			expect(y).toEqual([12, 9, 5, 2, 19]);
@@ -80,25 +98,131 @@ describe('InitiativeHelpers', () => {
 			const i = {
 				currentTurn: '555',
 				rolls: {
-					'111': generateInitiative(12),
-					'222': generateInitiative(5),
-					'333': generateInitiative(9),
-					'444': generateInitiative(19),
-					'555': generateInitiative(2)
+					'111': generateInitiative(12, 'aaa'),
+					'222': generateInitiative(5, 'bbb'),
+					'333': generateInitiative(9, 'ccc'),
+					'444': generateInitiative(19, 'ddd'),
+					'555': generateInitiative(2, 'eee')
 				}
 			};
-			const x = orderInitiatives(i);
+			const pc = generatePlayerCharacters(['aaa', 'bbb', 'ccc', 'ddd', 'eee']);
+			const npc = generateNonPlayerCharacters([]);
+			const x = orderInitiatives(i, pc, npc);
 
 			const y = x.map(z => z.initiativeRoll);
 			expect(y).toEqual([2, 19, 12, 9, 5]);
 		});
+		it('should resolve ties using the dexterity', () => {
+			const i = {
+				currentTurn: '333',
+				rolls: {
+					'111': generateInitiative(12, 'aaa'),
+					'222': generateInitiative(12, 'bbb'),
+					'333': generateInitiative(5, 'ccc')
+				}
+			};
+			const pc = generatePlayerCharacters(['aaa', 'bbb', 'ccc']);
+			const npc = generateNonPlayerCharacters([]);
+
+			pc[0].dexterity = 5;
+			pc[1].dexterity = 15; // Should win tie
+
+			const x = orderInitiatives(i, pc, npc);
+
+			const y = x.map(z => z.id);
+			expect(y).toEqual(['333', '222', '111']);
+		});
 	});
 });
 
-function generateInitiative(roll?: number): InitiativeRoller {
+// Generator functions
+
+function generateInitiative(roll?: number, pcId?: string, npcId?: string): InitiativeRoller {
 	return {
 		initiativeRoll: roll || Math.ceil(Math.random() * 20),
-		npcId: null,
-		pcId: null
+		npcId: npcId || null,
+		pcId: pcId || null
+	};
+}
+
+function generatePlayerCharacters(ids: string[]): PlayerCharacter[] {
+	return ids.map(x => generatePlayerCharacter(x));
+}
+
+function generateNonPlayerCharacters(ids: string[]): NonPlayerCharacter[] {
+	return ids.map(x => generateNonPlayerCharacter(x));
+}
+
+function generateNonPlayerCharacter(id: string): NonPlayerCharacter {
+	return {
+		...generateCharacter(id),
+		class: '',
+		environments: [],
+		source: '',
+		cr: 1,
+		ac: 15,
+		acType: '',
+		hp: 40,
+		maxHp: 60,
+		hpDice: '',
+		actions: [],
+		reactions: [],
+		legendaryActions: [],
+		legendaryActionCount: null,
+		languages: '',
+		saves: {},
+		damageResistances: '',
+		damageImmunities: '',
+		conditionImmunities: '',
+		traits: [],
+		senses: {},
+		skills: [],
+		variants: null
+	};
+}
+
+function generatePlayerCharacter(id: string): PlayerCharacter {
+	return {
+		...generateCharacter(id),
+		race: '',
+		ac: 15,
+		hp: 50,
+		maxHp: 70,
+		hitDice: 3,
+		levels: [],
+		equipment: [],
+		attacks: [],
+		spells: [],
+		saves: {},
+		skills: {},
+		traits: [],
+		resources: [],
+		spellSlots: {},
+		money: {
+			cp: 0,
+			ep: 0,
+			gp: 0,
+			pp: 0,
+			sp: 0
+		}
+	};
+}
+
+function generateCharacter(id: string): Character {
+	return {
+		id,
+		name: `testName-${id}`,
+		imageRef: '',
+		size: CharacterSize.Large,
+		strength: 10,
+		dexterity: 10,
+		constitution: 10,
+		intelligence: 10,
+		wisdom: 10,
+		charisma: 10,
+		speed: {
+			walk: 40
+		},
+		alignment: ''
 	};
 }
--- src/components/initiative/InitiativeHelpers.ts
+++ src/components/initiative/InitiativeHelpers.ts
@@ -1,21 +1,68 @@
 import { InitiativeRoller, InitiativeData } from '../../models/Initiative';
+import { PlayerCharacter, NonPlayerCharacter } from '../../5e/models/Character';
 
 /**
  * Given a collection of initiative rolls, sort them into the
  * correct order based on the indicated current turn.
  *
  * @param initiatives An array of initiatives to be sorted.
+ * @param pc An array of PlayerCharacters so ties can be solved using Character dexterity
+ * @param npc An array of NonPlayerCharacters so ties can be solved using Character dexterity
  * @returns A sorted array of InitiativeRoller
  */
-export const orderInitiatives = (initiatives: InitiativeData): InitiativeRoller[] => {
+export const orderInitiatives = (
+	initiatives: InitiativeData,
+	pc: PlayerCharacter[],
+	npc: NonPlayerCharacter[]
+): InitiativeRoller[] => {
 	if (!initiatives || !initiatives.rolls) {
+		return [];
+	}
+
+	if (!pc || !npc) {
 		return [];
 	}
 
 	const inits = { ...initiatives.rolls };
 	const initArray = Object.keys(inits).map(x => ({ ...inits[x], id: x }));
 
-	initArray.sort((a, b): number => b.initiativeRoll - a.initiativeRoll);
+	initArray.sort(
+		(a, b): number => {
+			const aChar = a.pcId
+				? pc.find(x => x.id === a.pcId)
+				: a.npcId
+				? npc.find(x => x.id === a.npcId)
+				: null;
+			const bChar = b.pcId
+				? pc.find(x => x.id === b.pcId)
+				: b.npcId
+				? npc.find(x => x.id === b.npcId)
+				: null;
+
+			// console.log(aChar);
+			// console.log(bChar);
+
+			if (!aChar || !bChar) {
+				// We cannot use Dex to compare these so rely on a simple sort
+				// console.log(
+				// 	`Cannot sort initiatives using dex tie-breaker as a or b don't have a char`
+				// );
+				return b.initiativeRoll - a.initiativeRoll;
+			}
+
+			console.log(
+				`${b.initiativeRoll} + 0.1 * ${bChar.dexterity} - (${a.initiativeRoll} + 0.1 * ${
+					aChar.dexterity
+				})`
+			);
+
+			return (
+				b.initiativeRoll +
+				0.1 * bChar.dexterity -
+				(a.initiativeRoll + 0.1 * aChar.dexterity)
+			);
+		}
+	);
 
 	const currTurnIdx = initArray.findIndex((x): boolean => x.id === initiatives.currentTurn);
 
