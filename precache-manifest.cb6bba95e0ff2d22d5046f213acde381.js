d5010345641d2553984aef61cd2a3db1b565df6d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-22 22:18:30 +0000

    Deploy dnd-player.com to github.com/dndplayer/dnd-player.git:gh-pages

--- precache-manifest.cb6bba95e0ff2d22d5046f213acde381.js
+++ precache-manifest.cb6bba95e0ff2d22d5046f213acde381.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "031fece181a56fc0543c",
+    "revision": "48a4df2f30f3706d2b9a",
     "url": "/static/css/main.fc881bfd.chunk.css"
   },
   {
-    "revision": "031fece181a56fc0543c",
-    "url": "/static/js/main.8c9d01d3.chunk.js"
+    "revision": "48a4df2f30f3706d2b9a",
+    "url": "/static/js/main.59bd929f.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
@@ -32,7 +32,7 @@ self.__precacheManifest = [
     "url": "/static/media/Ac.811dba49.svg"
   },
   {
-    "revision": "700211a674b3ac6312ca31c0208a9468",
+    "revision": "ebdd9bd9bef95b4bb16007fbb8e46640",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.52085e8184c9da8a170f837800e8352b.js"
+  "/precache-manifest.cb6bba95e0ff2d22d5046f213acde381.js"
 );
 
 workbox.clientsClaim();
