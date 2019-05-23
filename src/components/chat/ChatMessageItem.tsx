7c39f0f6fc3e7df81270d53fcead5c8b6d3d2689
Dom Light <bodricklight@gmail.com>
2019-05-24 00:10:09 +0100

    feat(chat): linkify urls sent in chat messages
    Closes #70

--- src/components/chat/ChatMessageItem.tsx
+++ src/components/chat/ChatMessageItem.tsx
@@ -8,13 +8,30 @@ interface Props {
 }
 export default class ChatMessageItem extends React.Component<Props> {
 	render(): ReactNode {
-		const { message, isOwner, styles } = this.props;
+		const { message, styles } = this.props;
 		const username = message.sender || 'unknown';
+		let tokens = (message.msg || '').split(/\s/);
+
+		const contents = tokens.map((token, i) => {
+			let hasSpace = i !== tokens.length - 1;
+			let maybeSpace = hasSpace ? ' ' : '';
+
+			if (token.match(/^https?\:\//)) {
+				return (
+					<a key={i} href={token} target="_blank">
+						{token}
+						{maybeSpace}
+					</a>
+				);
+			} else {
+				return token + maybeSpace;
+			}
+		});
 
 		return (
 			<div className={styles.message} key={message.id}>
 				<div className={styles.username}>{username} :</div>
-				<div>{message.msg}</div>
+				<div>{contents}</div>
 			</div>
 		);
 	}
